76430a21a1913c48b8bc14ed89527711f57d990f
Bruno Sola <brunosola3@gmail.com>
2023-12-01 18:32:29 -0300

    OrderStatus enum

--- src/main/java/com/brunosola/Store/config/TestConfig.java
+++ src/main/java/com/brunosola/Store/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.brunosola.Store.config;
 
 import com.brunosola.Store.entities.Order;
 import com.brunosola.Store.entities.User;
+import com.brunosola.Store.entities.enums.OrderStatus;
 import com.brunosola.Store.repositories.OrderRepository;
 import com.brunosola.Store.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +28,8 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.SHIPPED, user2);
 
         userRepository.saveAll(Arrays.asList(user1,user2));
         orderRepository.saveAll(Arrays.asList(o1,o2));
